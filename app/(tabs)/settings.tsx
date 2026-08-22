093dfb267fcb6ea8d7c9fb1ed43dfec6e0e6b2b1
João Vitor R. da Silva <joaovitor.r.silva.3@gmail.com>
2026-08-22 15:58:14 -0300

    Adiciona barra de navegação customizada.

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {Text} from "react-native";
+
+import {styled} from "nativewind"
+import {SafeAreaView as RNSafeAreaView} from 'react-native-safe-area-context'
+
+const SafeAreaView = styled(RNSafeAreaView)
+
 
 const Settings = () => {
-  return (
-    <View>
-      <Text>Settings</Text>
-    </View>
-  );
+    return (
+        <SafeAreaView className={'bg-background flex-1 p-5'}>
+            <Text>Settings</Text>
+        </SafeAreaView>
+    );
 };
 
 export default Settings;
--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {Text} from "react-native";
+import {styled} from "nativewind"
+import {SafeAreaView as RNSafeAreaView} from 'react-native-safe-area-context'
+
+const SafeAreaView = styled(RNSafeAreaView)
 
 const Subscriptions = () => {
-  return (
-    <View>
-      <Text>Subscriptions</Text>
-    </View>
-  );
+    return (
+        <SafeAreaView className={'bg-background flex-1 p-5'}>
+            <Text>Subscriptions</Text>
+        </SafeAreaView>
+    );
 };
 
 export default Subscriptions;
