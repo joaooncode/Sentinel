57c6d8d57979949f5fdf6185b7900e04ef3c416f
João Vitor R. da Silva <joaovitor.r.silva.3@gmail.com>
2026-08-22 01:38:26 -0300

    Configuração inicial do projeto, dependencias e design system

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-red-500">Sentinel - Your Expense Tracker</Text>
+    </View>
+  );
+}
